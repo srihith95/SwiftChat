218ce7c30fd75ad33ab89f634f69887ee953975a
srihith95 <103873053+srihith95@users.noreply.github.com>
2022-09-22 17:07:31 +0530

    First iOS commit

---
+++ SwiftText/SwiftText/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftText
+//
+//  Created by Srihith Prudhivi on 22/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
